c52e72066a66d6bdc27edb6478891f6cab99b685
aslafy_z <aslafy_z@epitech.eu>
2015-06-19 22:45:13 +0200

    updated imap protocol

--- config.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-import yaml
-
-with open('config.yaml', 'r') as stream:
-    print(yaml.load(stream))
--- imap.py
+++ imap.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python3
 
+##########
+import yaml
+config = None
+with open('config.yaml', 'r') as stream:
+    config = yaml.load(stream)
+##########
+
+print (config["MonCompte"]["username"])
+
 import imaplib
 import re
+
 
 class IMAPClient(object):
     connection = None
     list_response_pattern = re.compile(r'\((?P<flags>.*?)\) "(?P<delimiter>.*)" (?P<name>.*)')
 
-    def __init__(self, hostname):
-        self.connection = imaplib.IMAP4_SSL(hostname)
+    def __init__(self, hostname, ssl=True):
+        if ssl: self.connection = imaplib.IMAP4_SSL(hostname)
+        else: self.connection = imaplib.IMAP4(hostname)
 
     def login(self, username, password):
         self.connection.login(username, password)
@@ -22,9 +33,9 @@ class IMAPClient(object):
             mailbox_name = mailbox_name.strip('"')
             return (flags, delimiter, mailbox_name)
         typ, mlist = self.connection.list(**kwargs)
-        if typ == 'OK': return [parse_list(line)[2] for line in mlist]
+        if typ == 'OK': return [parse_list(line.decode("utf-8"))[2] for line in mlist]
         return None
 
-imap = IMAPClient("imap.gmail.com")
-imap.login("zadkiel.aharonian@gmail.com", "****")
+imap = IMAPClient(config["MonCompte"]["hostname"], ssl=config["MonCompte"]["ssl"])
+imap.login(config["MonCompte"]["username"], config["MonCompte"]["password"])
 print(imap.list())
