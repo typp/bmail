3c36096c3521c271b41503da8c459f0f6f054fad
la-val_c <la-val_c@epitech.eu>
2015-06-20 14:48:26 +0000

    profile

--- Dialog_NewProfile.py
+++ Dialog_NewProfile.py
@@ -8,8 +8,22 @@ from PyQt5 import QtCore
 from PyQt5 import QtWidgets
 from PyQt5 import uic
 
-class Dialog_NewProfile(QDialog):
-    def __init__(self):
+class Dialog_NewProfile (QDialog):
+    def __init__ (self, parent):
         QDialog.__init__(self)
         dirname = os.path.dirname(os.path.abspath(__file__))
         uic.loadUi(os.path.join(dirname, "dialog_newprofile.ui"), self)
+        self.parent = parent
+        self.buttonBox.accepted.connect(self.OK)
+
+    def OK (self):
+        method = "POP3" if self.input_radio_POP3.isChecked() else "IMAP"
+        self.parent.registerProfile(self.input_ProfileName.text(), {
+            'host': self.input_Host.text(),
+            'post': self.input_Port.value(),
+            'protocol': method,
+            'username': self.input_Username.text(),
+            'password': self.input_Password.text(),
+            'passRemember': self.input_checkbox_RememberPass.isChecked(),
+            'ssl': self.input_checkbox_SSL.isChecked()
+        })
--- MainWindow.py
+++ MainWindow.py
@@ -2,6 +2,8 @@
 
 import sys
 import os
+import re
+import yaml
 
 # try:
 #     from PySide import QtCore
@@ -18,13 +20,64 @@ from PyQt5 import uic
 
 from Dialog_NewProfile import *
 
-class MainWindow(QMainWindow):
-    def __init__(self):
+class MainWindow (QMainWindow):
+    def __init__ (self):
         QtWidgets.QMainWindow.__init__(self)
         dirname = os.path.dirname(os.path.abspath(__file__))
         uic.loadUi(os.path.join(dirname, "mainwindow.ui"), self)
+        self.action_profiles = []
         self.action_New.triggered.connect(self.newProfileDialog)
+        self.profileDir = 'profile'
 
-    def newProfileDialog(self):
-        dialog = Dialog_NewProfile()
+    def newProfileDialog (self):
+        dialog = Dialog_NewProfile(self)
         dialog.exec()
+
+    def appendProfile (self, name, entry_value):
+        self.action_No_profile_known.setVisible(False)
+        action = QtWidgets.QAction(self)
+        action.setText(name)
+        action.setObjectName(entry_value)
+        self.action_profiles.append(QtWidgets.QAction(self))
+        self.menuProfile.addAction(action)
+
+    def getProfileID (self):
+        files = os.listdir(self.profileDir)
+        profile_id = 1
+        used_ids = []
+        for name in files:
+            try:
+                used_ids += int(name.split('-'))
+            except:
+                pass
+        while profile_id in used_ids:
+            profile_id += 1
+        return profile_id
+
+    def getProfileFile (self, profile_id, name):
+        safe_name = re.sub(r"[^A-Za-z0-9-]", "-", name)
+        return os.path.join(
+            self.profileDir,
+            "%03d-%s.yaml" % (profile_id, safe_name))
+
+    def registerProfile (self, name, config):
+        """
+        config.host: string
+        config.post: integer
+        config.protocol: "POP3" | "IMAP"
+        config.username: string
+        config.password: string
+        config.passRemember: boolean
+        config.ssl: boolean
+        """
+        profile_id = self.getProfileID()
+        filename = self.getProfileFile(profile_id, name)
+        with open(filename, 'w') as stream:
+            stream.write(yaml.dump({
+                'id': profile_id,
+                'name': name,
+                'config': config
+            }))
+        self.appendProfile(name, filename)
+
+
