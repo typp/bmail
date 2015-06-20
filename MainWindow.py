fff3555b1d026fadb18d1fe7b7b2beae586ad2cc
la-val_c <la-val_c@epitech.eu>
2015-06-20 19:27:31 +0000

    main window + pop3 up

--- MainWindow.py
+++ MainWindow.py
@@ -5,6 +5,8 @@ import os
 import re
 import yaml
 
+from functools import partial
+
 # try:
 #     from PySide import QtCore
 #     from PySide import QtWidgets
@@ -14,9 +16,11 @@ import yaml
 #     from PyQt5 import QtWidgets
 
 from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QListWidgetItem
 from PyQt5 import QtCore
 from PyQt5 import QtWidgets
 from PyQt5 import uic
+from PyQt5 import QtGui
 
 from Dialog_NewProfile import *
 from MailboxPOP3 import *
@@ -26,10 +30,11 @@ class MainWindow (QMainWindow):
         QMainWindow.__init__(self)
         dirname = os.path.dirname(os.path.abspath(__file__))
         uic.loadUi(os.path.join(dirname, "mainwindow.ui"), self)
-        self.action_profiles = []
+        self.profiles = []
         self.action_New.triggered.connect(self.newProfileDialog)
         self.profileDir = 'profile'
         self.currentProfile = None
+        self.currentProfileAction = None
         self.updateAllProfiles()
 
     def updateAllProfiles (self):
@@ -42,19 +47,21 @@ class MainWindow (QMainWindow):
             with open(path, 'r') as stream:
                 profile = yaml.load(stream)
             if profile:
-                self.appendProfile(profile['name'], os.path.basename(path))
+                self.appendProfile(profile, os.path.basename(path))
 
     def newProfileDialog (self):
         dialog = Dialog_NewProfile(self)
         dialog.exec()
 
-    def appendProfile (self, name, entry_value):
+    def appendProfile (self, profile, filename):
         self.action_No_profile_known.setVisible(False)
         action = QtWidgets.QAction(self)
-        action.setText(name)
-        action.setObjectName(entry_value)
-        self.action_profiles.append(QtWidgets.QAction(self))
+        action.setText(profile['name'])
+        action.setObjectName('action_' + filename)
+        action.triggered.connect(partial(self.selectProfile, action, profile))
+        self.profiles.append(profile)
         self.menuProfile.addAction(action)
+        return action
 
     def getProfileID (self):
         files = os.listdir(self.profileDir)
@@ -90,12 +97,39 @@ class MainWindow (QMainWindow):
             self.currentProfile = MailboxPOP3(config)
         profile_id = self.getProfileID()
         filename = self.getProfileFile(profile_id, name)
+        profile = {
+            'id': profile_id,
+            'name': name,
+            'config': config
+        }
         with open(filename, 'w') as stream:
-            stream.write(yaml.dump({
-                'id': profile_id,
-                'name': name,
-                'config': config
-            }))
-        self.appendProfile(name, filename)
+            stream.write(yaml.dump(profile))
+        action = self.appendProfile(profile, filename)
+        self.selectProfile(action, profile)
 
+    def selectProfile (self, action, profile):
+        if not self.currentProfile or profile['id'] != self.currentProfile['id']:
+            if profile['config']['protocol'] == "POP3":
+                self.mailbox = MailboxPOP3(profile)
+            else:
+                return
+            print('couille')
+            self.mailbox.sync()
+            while self.mailList.count() > 0:
+                self.mailList.takeItem(0)
+            for message in self.mailbox.list():
+                item = QListWidgetItem(message['header']['subject'])
+                self.mailList.addItem(item)
+                self.mailList.itemClicked.connect(partial(self.showMail, message['id']))
+            if self.currentProfileAction:
+                font = QtGui.QFont()
+                font.setBold(False)
+                self.currentProfileAction.setFont(False)
+            self.currentProfileAction = action
+            font = QtGui.QFont()
+            font.setBold(True)
+            self.currentProfileAction.setFont(font)
+            self.currentProfile = profile
 
+    def showMail (self, item, mailno):
+        print('bite')
