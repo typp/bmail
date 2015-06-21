41b1037c663ccc799fe06b2965d6dd557dfbb13a
aslafy_z <aslafy_z@epitech.eu>
2015-06-21 22:42:14 +0200

    added py-qt4

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ PYTHON_PKGS=(PyYAML)
 virtualenv -p /usr/bin/python3.3 $PWD/venv
 source $PWD/venv/bin/activate
 
-cp -r /usr/lib64/python3.3/site-packages/{PyQt4,sip.so} venv/lib/python3.3/site-packages/
+cp -r libs/{PyQt4,sip.so} venv/lib/python3.3/site-packages/
 
 for pkg in ${PYTHON_PKGS[*]}
 do
