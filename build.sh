9bcae54c5a0bdd953e810e81385e641ea82fd503
aslafy_z <aslafy_z@epitech.eu>
2015-06-21 19:15:26 +0200

    catched ctrl-c & updated build script

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+# !/bin/bash
 
 PYTHON_PKGS=(PyYAML)
 
-virtualenv -p /usr/bin/python2.7 $PWD/venv
+virtualenv -p /usr/bin/python3 $PWD/venv
 source $PWD/venv/bin/activate
+
+cp -r /usr/lib/python3.4/site-packages/{PyQt5,sip.so} venv/lib/python3.4/site-packages/
 
 for pkg in ${PYTHON_PKGS[*]}
 do
      pip install --disable-pip-version-check --isolated --no-cache-dir $pkg
 done
-
